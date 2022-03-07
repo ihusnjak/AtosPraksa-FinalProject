7f8acaf90a34443db150a1dac84d6f8c5377339d
Franjo Filipović <ffilipovic@etfos.hr>
2022-03-07 18:58:20 +0000

    Replace gameScreen.py with Tkinter_GUI_TTT.py

--- gameScreen.py
+++ gameScreen.py
@@ -1,28 +1,92 @@
 from msilib.schema import Font
 from tkinter import *
 import tkinter.font as font
-from turtle import color, width
+from turtle import color
 import sys
 
+
+#define constant
 windowWidth = 400
 windowHeight = 400
-btnWidth = 100
-btnHeight = 50
-fieldBtnDim = 2
-initialXY = 80
+btnWidth = 300
+btnHeight = 70
+initialXY = 50
+incrementY = btnHeight + initialXY
+gsBtnWidth = 100
+gsBtnHeight = 50
+gsFieldBtnDim = 2
+gsInitialXY = 80
+popupWinWidth = 300
+popupWinHeight = 50
 backgroundColor = "#C1E3FF"
 
 btnBgColor = "#7CC6FE"
 btnFgColor = "#FFFFFF"
 btnActiveColor = "#3B83BA"
 
+
 def setBtnStyle(btn):
     btn["bg"] = btnBgColor
     btn["fg"] = btnFgColor
     btn["activebackground"] = btnActiveColor
     btn["activeforeground"] = btnFgColor
-    btn["font"] = ('arial bold', 14)
+    btn["font"] = ('arial bold', 22)
 
+class MainMenuScreen(Frame):
+    def __init__(self, container):
+        super().__init__(container)
+        #frame size and color
+        self.frame = Frame(container, background=backgroundColor, width=windowWidth, height=windowHeight).place(x=0, y=0)
+        
+        #define buttons
+        self.btnTictactoe = Button(self.frame,
+         text="Tic Tac Toe", 
+         command=lambda:[self.destroy(), TictactoeScreen(app)])
+        self.btnExit = Button(self.frame,
+         text="Exit",
+         command=self.quitProgram)
+        
+        #style buttons
+        for btn in (self.btnTictactoe, self.btnExit):
+            setBtnStyle(btn)
+
+        #place buttons
+        self.btnTictactoe.place(x=initialXY, y=initialXY, width=btnWidth, height=btnHeight)
+        self.btnExit.place(x=initialXY, y=initialXY + incrementY, width=btnWidth, height=btnHeight)
+    
+    def quitProgram(self):
+        sys.exit(0)
+
+class TictactoeScreen(Frame):
+    def __init__(self, container):
+        super().__init__(container)
+        #frame size and color
+        self.frame = Frame(container, background=backgroundColor, width=windowWidth, height=windowHeight).place(x=0, y=0)
+        
+        #define buttons
+        self.btnPvP = Button(self.frame,
+         text="Player vs Player", 
+         command=lambda:[self.destroy(), GameScreen(app)])
+        self.btnPvAI = Button(self.frame,
+         text="Play vs BOT",
+          command=lambda: self.startPvAIGame)
+        self.btnBack = Button(self.frame,
+         text="Back",
+          command=lambda:[self.destroy(), MainMenuScreen(app)])
+        
+        #style buttons
+        for btn in (self.btnPvP, self.btnPvAI, self.btnBack):
+            setBtnStyle(btn)
+
+        #place buttons
+        self.btnPvP.place(x=initialXY, y=initialXY, width=btnWidth, height=btnHeight)
+        self.btnPvAI.place(x=initialXY, y=initialXY + incrementY, width=btnWidth, height=btnHeight)
+        self.btnBack.place(x=initialXY, y=initialXY + 2*incrementY, width=btnWidth, height=btnHeight)
+
+    #needs to be implemented
+    def startPvAIGame():
+        return
+    
 class GameScreen(Frame):
     def __init__(self, container):
         super().__init__(container)
@@ -30,15 +94,15 @@ class GameScreen(Frame):
         self.frame = Frame(container, background=backgroundColor, width=windowWidth, height=windowHeight).place(x=0, y=0)
         
         #define board buttons
-        self.btnB1 = Button(self.frame, text=" ", font=("arial bold", 20), height=fieldBtnDim, width=fieldBtnDim*2, command=lambda: self.btnClick)
-        self.btnB2 = Button(self.frame, text=" ", font=("arial bold", 20), height=fieldBtnDim, width=fieldBtnDim*2, command=lambda: self.btnClick)
-        self.btnB3 = Button(self.frame, text=" ", font=("arial bold", 20), height=fieldBtnDim, width=fieldBtnDim*2, command=lambda: self.btnClick)
-        self.btnB4 = Button(self.frame, text=" ", font=("arial bold", 20), height=fieldBtnDim, width=fieldBtnDim*2, command=lambda: self.btnClick)
-        self.btnB5 = Button(self.frame, text=" ", font=("arial bold", 20), height=fieldBtnDim, width=fieldBtnDim*2, command=lambda: self.btnClick)
-        self.btnB6 = Button(self.frame, text=" ", font=("arial bold", 20), height=fieldBtnDim, width=fieldBtnDim*2, command=lambda: self.btnClick)
-        self.btnB7 = Button(self.frame, text=" ", font=("arial bold", 20), height=fieldBtnDim, width=fieldBtnDim*2, command=lambda: self.btnClick)
-        self.btnB8 = Button(self.frame, text=" ", font=("arial bold", 20), height=fieldBtnDim, width=fieldBtnDim*2, command=lambda: self.btnClick)
-        self.btnB9 = Button(self.frame, text=" ", font=("arial bold", 20), height=fieldBtnDim, width=fieldBtnDim*2, command=lambda: self.btnClick)
+        self.btnB1 = Button(self.frame, text=" ", font=("arial bold", 20), height=gsFieldBtnDim, width=gsFieldBtnDim*2, command=lambda: self.btnClick)
+        self.btnB2 = Button(self.frame, text=" ", font=("arial bold", 20), height=gsFieldBtnDim, width=gsFieldBtnDim*2, command=lambda: self.btnClick)
+        self.btnB3 = Button(self.frame, text=" ", font=("arial bold", 20), height=gsFieldBtnDim, width=gsFieldBtnDim*2, command=lambda: self.btnClick)
+        self.btnB4 = Button(self.frame, text=" ", font=("arial bold", 20), height=gsFieldBtnDim, width=gsFieldBtnDim*2, command=lambda: self.btnClick)
+        self.btnB5 = Button(self.frame, text=" ", font=("arial bold", 20), height=gsFieldBtnDim, width=gsFieldBtnDim*2, command=lambda: self.btnClick)
+        self.btnB6 = Button(self.frame, text=" ", font=("arial bold", 20), height=gsFieldBtnDim, width=gsFieldBtnDim*2, command=lambda: self.btnClick)
+        self.btnB7 = Button(self.frame, text=" ", font=("arial bold", 20), height=gsFieldBtnDim, width=gsFieldBtnDim*2, command=lambda: self.btnClick)
+        self.btnB8 = Button(self.frame, text=" ", font=("arial bold", 20), height=gsFieldBtnDim, width=gsFieldBtnDim*2, command=lambda: self.btnClick)
+        self.btnB9 = Button(self.frame, text=" ", font=("arial bold", 20), height=gsFieldBtnDim, width=gsFieldBtnDim*2, command=lambda: self.btnClick)
         
         #other buttons
         self.btnReset = Button(self.frame,
@@ -64,8 +128,8 @@ class GameScreen(Frame):
         self.btnB9.grid(row=3, column=3, padx=(0, 0))
             
         #place buttons
-        self.btnReset.place(x=initialXY, y=4*initialXY, width=btnWidth, height=btnHeight)
-        self.btnBack.place(x=initialXY + 132, y=4*initialXY, width=btnWidth, height=btnHeight)
+        self.btnReset.place(x=gsInitialXY, y=4*gsInitialXY, width=gsBtnWidth, height=gsBtnHeight)
+        self.btnBack.place(x=gsInitialXY + 132, y=4*gsInitialXY, width=gsBtnWidth, height=gsBtnHeight)
     
     #needs to be implemented
     def resetGame():
@@ -73,9 +137,21 @@ class GameScreen(Frame):
     #needs to be implemented
     def btnClick():
         return
-     
-class TictactoeScreen():
-    pass
+        
+#popup window triggered when game ended
+class PopupMessage:
+    def __init__(self, master):
+        super().__init__()
+        #global top
+        top = Toplevel(master, background=backgroundColor, width=popupWinWidth, height=popupWinHeight)
+        
+        resultMessage = gameEndMessage()
+        message = Label(top, text=resultMessage, font=("arial bold", 20))
+        message.pack(padx=25, pady=25)
+        
+#needs to be implemented
+def gameEndMessage():
+    return
 
 class App(Tk):
     def __init__(self):
@@ -84,10 +160,9 @@ class App(Tk):
         self.title('AI GAMES')
         self.geometry(str(windowWidth) + "x" + str(windowHeight))
         self.resizable(False, False)
-
- 
+        
 if __name__ == "__main__":
     global app
     app = App()
-    frame = GameScreen(app)
+    frame = MainMenuScreen(app)
     app.mainloop()
