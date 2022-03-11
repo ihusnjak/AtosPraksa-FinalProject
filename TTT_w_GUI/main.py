31a2e89c209528ffb078d15e4a6caa598276e2d1
Mihael Španović <span.mihael@gmail.com>
2022-03-11 09:13:13 +0100

    Update documentation

--- TTT_w_GUI/main.py
+++ TTT_w_GUI/main.py
@@ -21,18 +21,24 @@ First the PvP game:
     and Player 2 objects. Using GameLogic object we initialize play() method. play() method is defined in game_logic.py file and as a parametar we give it a number that 
     represents our wanted gamemode and play order. In Player vs Player order doesn't matter but gamemode does. 
 
-    **In game_logic file there are explanations of what every method does in case you want to make changes.
+    **In game_logic file there are explanations of what every method does in case you want to make direct changes.
 
-    Here is the list of current gamemodes and orders:   0- Player VS Player
-                                                        1- Player VS AI
-                                                        2- AI VS Player
+    Here is the list of current gamemodes and orders with their respective index:   0- Player VS Player
+                                                                                    1- Player VS AI
+                                                                                    2- AI VS Player
 
     Initialization of play() method creates GameScreen class object and now we have a tic tac toe game board to play with.
 
     After selecting gamemode by clicking on a button in previous menu we also give reset_state variable value of 1 or 2 depending on the chosen menu.
     reset_state is important because it allows us to remeber the selected game mode so that in case where we want to restart the game we don't have to re-select the gamemode.
 
-    ..... TO BE CONTINUED
+    In game_logic file 3 funcations are mainly used for PvP gamemode: humanPlayerAction(), changeButtonState() and checkWin().
+    These three are running interchangeably in give order between players. humanPlayerAction() method takes as an argument a number which defines the order we want to take.
+    For example humanPlayerAction(1) means that we want to play first. It returns a tuple of row and col values "(row,col)" which is given to the changeButtonState() method located in game_screen.py
+    changeButtonState takes the given tuple and based on its value marks that button as either X or O for a visual representation of our choice. Last method is checkWin() and its job is to check for
+    any win conditions(winning combinations are: 3x/3o in either row, col or one of two diagonals).
+
+    Process repeats itself until either a win or a tie is achieved.
 
 
 
