2f409f3a97613a1d31826a1392ebe63112295ab6
Franjo Filipović <ffilipovic@etfos.hr>
2022-03-14 21:51:38 +0000

    Changed popups.py

--- TTT_w_GUI/popups.py
+++ TTT_w_GUI/popups.py
@@ -1,17 +1,21 @@
 from constants import *
 
-
 """
 Class PopupMessage is currently only used to display winner name after game is finished
 
 Args: You need to pass app which is a tkinter object and your message --> Currently hardcoded to only show winner message
 """
+        
 class PopupMessage():
-    def __init__(self, master,message):
+    def __init__(self, root, message):
         super().__init__()
-        #global top
+        
         self.message = message
-        top = Toplevel(master, background=backgroundColor, width=popupWinWidth, height=popupWinHeight)
+        top = Toplevel(background=backgroundColor, width=popupWinWidth, height=popupWinHeight)
+        top.transient(root)
+        top.grab_set()
+        #root.wait_window(top)
+        
         global result
         
         if(message == "Tie"):
@@ -19,6 +23,6 @@ class PopupMessage():
         else:
             message = "Winner is: {}!".format(message)
 
-        popmessage = Label(top, text=message, font=("arial bold", 20))
+        popmessage = Label(top, text=message, font=("arial bold", 20), bg=backgroundColor)
         popmessage.pack(padx=25, pady=25)
         result = " "
