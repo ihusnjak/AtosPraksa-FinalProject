997d7fc3a9e233354e47073568ee0897dcc44c34
DocMinike <dominik.majdandzic@gmail.com>
2022-03-16 17:15:38 +0100

    removed redundant project

--- TicTacToe/app/src/main/java/hr/atos/praksa/tictactoe/listeners/OnMatchClickListener.java
+++
@@ -1,5 +0,0 @@
-package hr.atos.praksa.tictactoe.listeners;
-
-public interface OnMatchClickListener {
-    void onMatchClick(int position);
-}
--- TicTacToe/app/src/main/java/hr/atos/praksa/tictactoe/listeners/UiListener.java
+++
@@ -1,9 +0,0 @@
-package hr.atos.praksa.tictactoe.listeners;
-
-import java.util.ArrayList;
-
-import hr.atos.praksa.tictactoe.model.Match;
-
-public interface UiListener {
-    void onModificationPerformed(ArrayList<Match> matches);
-}
