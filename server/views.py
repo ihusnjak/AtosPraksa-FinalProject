8f6b99a646708136278c969b948bfa4c0db5ea82
Hrvoje Škrbina <hrvojesmsskrbina@gmail.com>
2022-03-17 19:34:21 +0100

    Added PostGame view

--- server/views.py
+++ server/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import GameSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class PostGame(APIView):
+	def post(self, request):
+		gameSerializer = GameSerializer(data=request.data, many=True)
+		if gameSerializer.is_valid():
+			gameSerializer.save()
+			return Response(gameSerializer.data, status=status.HTTP_201_CREATED)
+		return Response(gameSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
